65af604af076c36e012065de07491f9437784b02
archria <jonghyun95@kakao.com>
2022-01-06 16:29:41 +0900

    2022-01-06 백준

--- 1929.cpp
+++ 1929.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int main(){
@@ -7,8 +7,21 @@ int main(){
     cin>>m;
     cin>>n;
     
-    bool isPrime[1000000] = {false};
-    
+    bool isPrime[1000001] = {false};
+    isPrime[1] = true;
+    for(int i = 2; i < sqrt(1000000); i++){
+        if(isPrime[i] == false){
+            for(int j = i ; j < 1000000; j++){
+                if(i*j > 1000000) break;
+                isPrime[i*j] = true;
+            }
+        }
+    }
+    for(int i = m ; i <= n ; i++){
+        if(isPrime[i] == false)
+            cout<<i<<"\n";
+            // endl은 상당히 느리다 \n을 사용하자
+    }
 
     return 0;
 }
---
+++ 4948.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    bool isTimeNumber[246913] = {false};
+    for(int i = 2 ; i <= sqrt(246913) ; i++){
+        if(isTimeNumber[i] == false){
+            for(int j = 2 ; j < 246913 ; j++){
+                if(i*j > 246912) break;
+                isTimeNumber[i*j] = true;
+            }
+        }
+    }
+    while(true){
+        int n;
+        cin>>n;
+        int ans = 0;
+        for(int i = n+1 ; i <= 2*n ; i++){
+            if(isTimeNumber[i] == false)
+                ans++;
+        }
+        cout<<ans<<"\n";
+
+        
+    }
+
+    return 0;
+}
