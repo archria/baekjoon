d39e25d76b09adfa29d3709698f0528a2a564d8c
archria <jonghyun95@kakao.com>
2022-03-08 18:24:58 +0900

    2022-03-08

--- 1012.cpp
+++ 1012.cpp
@@ -1,9 +1,67 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
+int T;
+int h,w,n;
+int ans = 0;
+
+void bfs(int board[50][50], int i, int j){
+    queue<pair<int,int>> coor;
+    pair<int,int> xy = make_pair(i,j);
+    board[i][j] = 0;
+    coor.push(xy);
+    ans += 1;
+    //cout<<"baechoo : "<<ans<<endl;
+    while(!coor.empty()){
+        pair<int,int> top = coor.front();
+        //cout<<"coor : "<<top.first<<" "<<top.second<<endl;
+        coor.pop();
+        if(top.first > 0 && board[top.first-1][top.second] == 1){
+            pair<int,int> up = make_pair(top.first-1, top.second);
+            board[up.first][up.second] = 0;
+            coor.push(up);
+        }
+        if(top.first < w-1 && board[top.first+1][top.second] == 1){
+            pair<int,int> down = make_pair(top.first+1, top.second);
+            board[down.first][down.second] = 0;
+            coor.push(down);
+        }
+        if(top.second > 0 && board[top.first][top.second-1] == 1){
+            pair<int,int> left = make_pair(top.first, top.second-1);
+            board[left.first][left.second] = 0;
+            coor.push(left);
+        }
+        if(top.second < h-1 && board[top.first][top.second+1] == 1){
+            pair<int,int> right = make_pair(top.first, top.second+1);
+            board[right.first][right.second] = 0;
+            coor.push(right);
+        }
+    }
+}
 
 int main(){
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin>>T;
+    for(int i = 0; i < T ; i++){
+        cin>>w>>h>>n;
+        int board[50][50] = {0,};
+        for(int j = 0 ; j < n ; j++){
+            int a,b;
+            cin>>a>>b;
+            board[a][b] = 1;
+        }
+        ans = 0;
+        for(int a = 0; a < h ; a++){
+            for(int b = 0; b < w ; b++){
+                if(board[b][a] == 1){
+                    bfs(board,b,a);
+                }
+            }
+        }
+        cout<<ans<<"\n";
+    }
     
     return 0;
 }
---
+++ 2178.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+int map[100][100] = {0,};
+int n,m, step = 0;
+queue<pair<int,int>> primary;
+queue<pair<int,int>> secondary;
+
+int bfs();
+void fillqueue();
+
+int bfs(){
+    step += 1;
+    while(!primary.empty()){
+        pair<int,int> top = primary.front();
+        primary.pop();
+        if(top.first == n-1 && top.second == m-1){
+            return 0;
+        }
+        if(top.first > 0 && map[top.first-1][top.second] == 1){
+            pair<int,int> up = make_pair(top.first-1,top.second);
+            map[up.first][up.second] = 0;
+            secondary.push(up);
+        }
+        if(top.first < n-1 && map[top.first+1][top.second] == 1){
+            pair<int,int> down = make_pair(top.first+1,top.second);
+            map[down.first][down.second] = 0;
+            secondary.push(down);
+        }
+        if(top.second > 0 && map[top.first][top.second-1] == 1){
+            pair<int, int> left = make_pair(top.first, top.second-1);
+            map[left.first][left.second] = 0;
+            secondary.push(left);
+        }
+        if(top.second < m-1 && map[top.first][top.second+1] == 1){
+            pair<int, int> right = make_pair(top.first, top.second+1);
+            map[right.first][right.second] = 0;
+            secondary.push(right);
+        }                
+    }
+    fillqueue();
+}
+void fillqueue(){
+    while(!secondary.empty()){
+        primary.push(secondary.front());
+        secondary.pop();
+    }
+    bfs();
+}
+
+
+int main(){
+    cin>>n>>m;
+    for(int i = 0; i < n ; i++){
+        string temp;
+        cin>>temp;
+        for(int j = 0; j < m ; j++){
+            map[i][j] = temp[j]-'0';
+        }
+    }
+    pair<int,int> z = make_pair(0,0);
+    map[0][0] = 0;
+    primary.push(z);
+    bfs();
+    cout<<step;
+    return 0;
+}
