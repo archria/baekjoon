8d52d8203758b72a8c70d3f0a796936cf98afc5a
archria <jonghyun95@kakao.com>
2022-04-05 15:16:25 +0900

    2022-04-05

--- 1629.cpp
+++ 1629.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
+long long solve(long long a, long long b, long long m){
+    if(b == 0) return 1;
+    long long val = solve(a,b/2,m);
+    val = val * val % m;
+
+    if(b % 2 == 0) return val;
+    else
+        return (val * a) % m;
+}
+
 int main(){
     long long a,b,c;
     cin>>a>>b>>c;
-    for(int i = 0; i < b ; i++){
-        a *= a;
-        a = a%c;
-    }
+    
     cout<<a;
 
     return 0;
