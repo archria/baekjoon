bbd2156b53c211cf033094f4101be7c569f7ae4d
archria <jonghyun95@kakao.com>
2022-04-12 15:14:59 +0900

    2022-04-12

--- 1764.cpp
+++ 1764.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-long long hash(string temp){
-    long long ans = 0;
-    for(int i = 0; i < temp.length() ; i++){
-        ans += 
-    }
-
-    return ans;
-}
-
 int main(){
-
+    int n,m,ans=0;
+    cin>>n>>m;
+    vector<string> cont;
+    vector<string> anscont;
+    for(int i = 0; i < n+m; i++){
+        string temp;
+        cin>>temp;
+        cont.push_back(temp);
+    }
+    sort(cont.begin(), cont.end());
+    for(int i = 0; i < n+m ; i++){
+        if(cont[i] == cont[i+1]){
+            ans += 1;
+            anscont.push_back(cont[i]);
+            i+=1;
+        }
+    }
+    cout<<ans<<"\n";
+    for(int i = 0; i < anscont.size() ; i++){
+        cout<<anscont[i]<<"\n";
+    }
     return 0;
 }
