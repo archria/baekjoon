e2eebd9f2f3862ccfba6606b238c952085e30c63
archria <jonghyun95@kakao.com>
2022-04-25 17:57:56 +0900

    Create cpp_class_basic.cpp

---
+++ cpp_class_basic.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class myFuncClass{
+protected:
+    int fuel = 0;
+    bool power = false;
+
+public:
+    myFuncClass(){
+        int fuel = 100;
+        bool power = true;
+    }
+    myFuncClass(int n){
+        int fuel = n;
+        bool power = true;
+    }
+    void go(){
+        this->fuel--;
+    }
+    void gas_station(int n){
+        this->fuel += n;
+    }
+    void fuel_check(){
+        cout<<"fuel : "<<this->fuel<<endl;
+    }
+};
+
+class myChildClass : public myFuncClass{
+public:
+    void go(){
+        this->fuel -= 2;
+    }
+};
+
+int main(){
+    myChildClass suv = myChildClass();
+    suv.gas_station(6);
+    suv.fuel_check();
+    suv.go();
+    suv.fuel_check();
+
+}
