2f25a015dbaeaf2ffeabb453d98bdaf23802128d
archria <jonghyun95@kakao.com>
2022-04-27 18:30:28 +0900

    Update 1753.cpp

--- 1753.cpp
+++ 1753.cpp
@@ -1,12 +1,25 @@
-#include <iostream>b
+#include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
-
+int dist[20001];
+vector<pair<int,int>> vertex[20001];
 
 
 int main(){
-    
+    int V,E,K;
+    cin>>V>>E>>K;
+    for(int i = 0; i < E; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        vertex[a].push_back({b,c});
+    }
+    for(int i = 1; i <= V; i++){
+        dist[i] = 99999999;
+    }
+
+        
     
     return 0;
 }
